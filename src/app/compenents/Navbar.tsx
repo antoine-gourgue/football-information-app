26d25865bdf8689683d39a6f7f611aec8713a76b
Antoine <gourgue.antoine@esh64.fr>
2023-09-18 01:34:45 +0200

    fix: Navbar style

--- src/app/compenents/Navbar.tsx
+++ src/app/compenents/Navbar.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 
 const Navbar:FC = () => {
     return(
-        <div className='flex justify-between items-center py-2'>
+        <div className='flex justify-center items-center py-2'>
             <Link href='/' className='flex items-center space-x-2'>
                 <div className='relative w-[30px] h-[30px]'>
                     <Image src="/football-info.png" alt="logo" fill
                            className='object-cover'/>
                 </div>
-                <span className='text-2xl font-bold none md:block'>Football info</span>
+                <span className='text-2xl font-bold none md:block'>Football Score</span>
             </Link>
-            <p className='text-xs md:text-sm'>create by antoine gourgue</p>
+
         </div>
     )
 }
