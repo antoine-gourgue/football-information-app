f92cadf43afdaf54f83130dfa2adb03fdf5bf065
Antoine <gourgue.antoine@esh64.fr>
2023-09-17 16:15:00 +0200

    feat: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex text-textPrimary">
 
-    </main>
+  const nd = new Date();
+  const dateConvert = nd.toDateString()
+
+  return (
+    <section className="px-2 md:px-4 md:w-[600px]">
+      <div className="flex justify-between items-center mb-4 md:mb-2">
+        <h1 className="text-md md:text-xl font-bold">MATCHES</h1>
+        <div className="px-4 py-0 md:py-1 bg-slate-600 rounded-md text-textPrimary text-sm">
+          <p>{dateConvert}</p>
+        </div>
+      </div>
+      
+    </section>
   )
 }
